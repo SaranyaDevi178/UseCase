c99058217daf04be7515a60e3b2db384fe0a4790
unknown <cogdotnet1688@iiht.tech>
2022-09-15 21:25:57 +0530

    Digitalbook

---
+++ DigitalBooks/AuthServer/UserValidationRequestModel.cs
@@ -0,0 +1,20 @@
+﻿using AuthServer.DatabaseEntity;
+
+internal class UserValidationRequestModel
+{
+    private readonly DigitalbooksContext context = new DigitalbooksContext();
+
+    public string UserName { get; set; }
+    public string Password { get; set; }
+
+    public bool IsValidate(string username, string password)
+    {
+        if (context.UserTables.Any(x => x.UserName == username && x.Password == AuthServer.EncryptDecrypt.EncodePasswordToBase64(password)))
+           // AuthServer.EncryptDecrypt.EncodePasswordToBase64(password)
+           { 
+                return true;
+        }
+        else
+            return false;
+    }
+}
